35f10b1e2b80fe64f3c713f722fd5e86d5c30d4e
Yousof Algburi <75765571+yousofalgburi@users.noreply.github.com>
2022-04-08 00:31:05 -0400

    create frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import { ColorModeScript } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<><ColorModeScript/><App/></>, document.getElementById('root'))
